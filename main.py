80b9c690220641b1576167017059608f2bece3c3
Kunal <fergyfaltu@gmail.com>
2026-01-07 01:52:45 +0530

    mcp

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 con = sqlite3.connect(DB_PATH, check_same_thread=False)
+#hus nakjnsknnerjvnekn
 
 
 
